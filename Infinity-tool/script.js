054ee1ff14dbbbf82458fd1c0431c38b324bed07
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-08-16 16:35:13 +0900

    infinity tools

--- Infinity-tool/script.js
+++ Infinity-tool/script.js
@@ -1,71 +1,115 @@
 const Scratch_Infinity_framework = {
   source: {
     html: `
-    <section data-class="head">
-    <img width:24px; src="https://develop.the-infinitys.f5.si/image/The-Infinitys.webp" crossorigin="anonymous" />
-    <h1>Infinity tool</h1>
-    <button id="Scratch-Infinity-tools-max">⬜︎</button>
-    </section>
-    <div data-class="tools">
-      
-    </div>`,
+      <section data-class="head">
+      <img width:24px; src="https://develop.the-infinitys.f5.si/image/The-Infinitys.webp" crossorigin="anonymous" />
+      <h1>Infinity tool</h1>
+      <button id="Scratch-Infinity-tools-max">⬜︎</button>
+      </section>
+      <div data-class="tools">
+        <div data-class="tabs">
+          <button data-extension-name="welcome">welcome</button>
+        </div>
+        <div data-class="windows">
+          <div data-extension-name="welcome">
+            <h1>Welcome</h1>
+          </div>
+        </div>
+      </div>`,
     css: `
-    #Scratch-Infinity-tools{
-      position:fixed;
-      z-index:10000000000;
-      width:max(280px,30vw);
-      height:100%;
-      top:0;
-      right:0;
-      background-color:black;
-      color:white;
-      font-family:system-ui,sans-serif;
-    }
-    #Scratch-Infinity-tools>section[data-class="head"]{
-      display:flex;
-      flex-shrink:0;
-      width:100%;
-    }
-    #Scratch-Infinity-tools>section[data-class="head"]>img{
-      margin-top:3px;
-      margin-right:4px;
-      width:30px;
-      height:30px;
-    }
-    #Scratch-Infinity-tools>section[data-class="head"]>h1{
-      font-size:30px;
-      margin:0;
-    }
-    #Scratch-Infinity-tools-max{
-      position:fixed;
-      width:40px;
-      height:30px;
-      top:2px;
-      right:40px;
-      background-color:black;
-      color:white;
-      border:1px solid white;
-    }
-    #Scratch-Infinity-tools-min{
-      position:fixed;
-      z-index:100000000000;
-      width:40px;
-      height:30px;
-      top:2px;
-      right:0;
-      background-color:black;
-      color:white;
-      border:1px solid white;
-    }
-    #Scratch-Infinity-tools>div[data-class="tools"]{
-      position:absolute;
-      top:35px;
-      left:0;
-      width:100%;
-      height:calc(100%-35px);
-      background-color:#111;
-      color:white;
-    }
+      #Scratch-Infinity-tools{
+        position:fixed;
+        z-index:10000000000;
+        width:max(280px,30vw);
+        height:100%;
+        top:0;
+        right:0;
+        background-color:black;
+        color:white;
+        font-family:system-ui,sans-serif;
+      }
+      #Scratch-Infinity-tools>section[data-class="head"]{
+        display:flex;
+        flex-shrink:0;
+        width:100%;
+      }
+      #Scratch-Infinity-tools>section[data-class="head"]>img{
+        margin-top:3px;
+        margin-right:4px;
+        width:30px;
+        height:30px;
+      }
+      #Scratch-Infinity-tools>section[data-class="head"]>h1{
+        font-size:30px;
+        margin:0;
+      }
+      #Scratch-Infinity-tools-max{
+        position:fixed;
+        width:40px;
+        height:30px;
+        top:2px;
+        right:40px;
+        background-color:black;
+        color:white;
+        border:1px solid white;
+      }
+      #Scratch-Infinity-tools-min{
+        position:fixed;
+        z-index:100000000000;
+        width:40px;
+        height:30px;
+        top:2px;
+        right:0;
+        background-color:black;
+        color:white;
+        border:1px solid white;
+      }
+      #Scratch-Infinity-tools>div[data-class="tools"]{
+        position:absolute;
+        top:35px;
+        left:0;
+        width:100%;
+        height:calc(100% - 35px);
+        background-color:#333;
+        color:white;
+      }
+      #Scratch-Infinity-tools>div[data-class="tools"]>div[data-class="tabs"]{
+        position:absolute;
+        top:0;
+        left:2px;
+        width:calc(100% - 4px);
+        height:35px;
+        background-color:#111;
+        color:white;
+      }
+      #Scratch-Infinity-tools>div[data-class="tools"]>div[data-class="tabs"]>button{
+        position:relative;
+        width:100px;
+        height:35px;
+        border-radius:10px 10px 0 0;
+        background-color:#111;
+        color:white;
+      }
+      #Scratch-Infinity-tools>div[data-class="tools"]>div[data-class="windows"]{
+        position:absolute;
+        padding:0;
+        margin:0;
+        top:35px;
+        left:2px;
+        width:calc(100% - 4px);
+        height:calc(100% - 35px);
+        background-color:#fff;
+        color:white;
+      }
+      #Scratch-Infinity-tools>div[data-class="tools"]>div[data-class="windows"]>div{
+        position:absolute;
+        top:0;
+        left:0;
+        width:100%;
+        height:100%;
+        background-color:#000;
+        color:white;
+      }
     `,
   },
   display: {
